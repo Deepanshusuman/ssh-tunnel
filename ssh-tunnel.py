5acbcfd84eca0b095fc321a21cdd49ee23ef559f
Deepanshu Suman <Deepanshusuman0000@gmail.com>
2020-07-26 15:16:16 +0530

    Update ssh-tunnel.py

--- ssh-tunnel.py
+++ ssh-tunnel.py
@@ -1,7 +1,11 @@
 import subprocess
 import time
+import socket
+ip = socket.gethostbyname(socket.gethostname())
+cloudport =  3000
+localport = 80
 
-p = subprocess.Popen("ssh -R pivpn:8194:localhost:8194 serveo.net", shell=True, stdin=None, stdout=None, stderr=None, close_fds=False)
+p = subprocess.Popen("ssh -N -f -R 5000:+ip+:80 ubuntu@13.234.76.83 -i rsa_key", shell=True, stdin=None, stdout=None, stderr=None, close_fds=False)
 return_code = None
 while True:
     return_code = p.poll()
